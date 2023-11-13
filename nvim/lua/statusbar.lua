ad5aa240380ae317ca6983fb5af85c8192bd3712
Percy Bolmer <percy.bolmer@bliq.tech>
2023-11-13 16:30:28 +0100

    feat: add lazynvim instead of packer

--- nvim/lua/statusbar.lua
+++
@@ -1,8 +0,0 @@
-require('lualine').setup {
-  options = {
-    theme = 'tokyonight'
-    -- ... your lualine config
-  },
-  sections = {lualine_c = {require('auto-session-library').current_session_name}}
-}
-
--- nvim/lua/telescopecfg.lua
+++
@@ -1,8 +0,0 @@
-local builtin = require('telescope.builtin')
--- search files, even hidden ones
-vim.keymap.set('n', '<leader>ff', ':lua require"telescope.builtin".find_files({no_ignore=true, hidden=true})<CR>', {})
--- ripgrep files, respects gitignore
-vim.keymap.set('n', '<leader>fg', ':lua require"telescope.builtin".live_grep({no_ignore=true, hidden=true})<CR>', {})
-vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
-vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
-
